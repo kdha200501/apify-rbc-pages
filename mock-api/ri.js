5fefd485ee1051fa88101c4e3fa2e8aa404a423b
Jacks Diao <kdha200501@gmail.com>
2020-09-13 13:16:48 -0400

    add mock data

---
+++ mock-api/ri.js
@@ -0,0 +1,194 @@
+var riMapping = {
+	"registeredplans": [
+		{
+			id: "0006960006",
+			label: "$0 to $5,499.99",
+			value: "{v}"
+		}, 
+		{
+			id: "0006960007",
+			label: "$5,500 to $24,999.99",
+			value: "{v}"
+		},
+		{
+			id: "0006960008",
+			label: "$25,000 to $49,999.99",
+			value: "{v}"
+		},
+		{
+			id: "0006960009",
+			label: "$50,000 to $999,999,999.99",
+			value: "{v}"
+		}
+	],
+	"tfsa": [
+		{
+			id: "0230740002",
+			label: "$0 to $5,499.99",
+			value: "{v}"
+		},
+		{
+			id: "0230740003",
+			label: "$5,500 to $24,999.99",
+			value: "{v}"
+		},
+		{
+			id: "0230740004",
+			label: "$25,000 to $49,999.99",
+			value: "{v}"
+		},
+		{
+			id: "0230740005",
+			label: "$50,000 to $999,999,999.99",
+			value: "{v}"
+		},
+	],
+	"bonusinterest": [
+		{
+			id: "0372260006",
+			label: "not currently used",
+			value: "{v}"
+		},
+	],
+	"redeemablesemiannualcompoundgic": [
+		{
+			id: "0006980004",
+			label: "1 to 1.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980005",
+			label: "1.5 to 2 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980006",
+			label: "2 to 2.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980007",
+			label: "2.5 to 3 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980008",
+			label: "3 to 4 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980009",
+			label: "4 to 5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980010",
+			label: "5 years only",
+			value: "{v}"
+		}
+	],
+	"nonredeemableannualcompoundgic": [
+		{
+			id: "0061880001",
+			label: "30 to 89 days",
+			value: "{v}"
+		},
+		{
+			id: "0006980025",
+			label: "90 to 179 days",
+			value: "{v}"
+		},
+		{
+			id: "0006980026",
+			label: "180 to 269 days",
+			value: "{v}"
+		},
+		{
+			id: "0006980027",
+			label: "270 to 364 days",
+			value: "{v}"
+		},
+		{
+			id: "0006980028",
+			label: "1 to 1.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980029",
+			label: "1.5 to 2 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980030",
+			label: "2 to 2.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980031",
+			label: "2.5 to 3 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980032",
+			label: "3 to 4 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980033",
+			label: "4 to 5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980034",
+			label: "5 years only",
+			value: "{v}"
+		},
+		{
+			id: "0061880002",
+			label: "7 years only",
+			value: "{v}"
+		},
+		{
+			id: "0061880003",
+			label: "10 years only",
+			value: "{v}"
+		}
+	],
+	"nonredeemableannualnoncompoundgic": [
+		{
+			id: "0006980018",
+			label: "1 to 1.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980019",
+			label: "1.5 to 2 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980020",
+			label: "2 to 2.5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980021",
+			label: "2.5 to 3 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980022",
+			label: "3 to 4 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980023",
+			label: "4 to 5 years less a day",
+			value: "{v}"
+		},
+		{
+			id: "0006980024",
+			label: "5 years only",
+			value: "{v}"
+		}
+	]
+}
