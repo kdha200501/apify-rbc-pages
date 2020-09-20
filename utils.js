3e2e32ec873512c941f9355afcd1f7664548dd64
Jacks Diao <kdha200501@gmail.com>
2020-09-20 09:34:42 -0400

    run eslint

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-const { readFile } = require("fs");
-const https = require("https");
-const { load, html } = require("cheerio");
-const { chunk, zipObject, sortBy } = require("lodash");
+const { readFile } = require('fs');
+const https = require('https');
+const { load, html } = require('cheerio');
+const { chunk, zipObject, sortBy } = require('lodash');
 
-const { regexpNewLine } = require("./const");
+const { regexpNewLine } = require('./const');
 
 function readMockFile(filePath) {
   return new Promise((resolve, reject) => {
-    readFile(filePath, "utf8", (err, data) => {
+    readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         reject(err);
         return;
@@ -24,24 +24,24 @@ function fetch(url) {
         let data = [];
 
         // a chunk of data is received
-        resp.on("data", (chunk) => data.push(chunk));
+        resp.on('data', (chunk) => data.push(chunk));
 
         // the final chunk of data received
-        resp.on("end", () => resolve(data.join("")));
+        resp.on('end', () => resolve(data.join('')));
       })
-      .on("error", ({ message }) => reject(message));
+      .on('error', ({ message }) => reject(message));
   });
 }
 
 function extractTable($) {
-  const hasThead = $("thead").length > 0;
+  const hasThead = $('thead').length > 0;
 
   const keyCell = Array.from(
-    $(hasThead ? "thead th" : "tr:first-of-type td, tr:first-of-type th")
+    $(hasThead ? 'thead th' : 'tr:first-of-type td, tr:first-of-type th')
   )
     .map(html)
     .map(load)
-    .map((cellElement) => cellElement.text().replace(regexpNewLine, "").trim());
+    .map((cellElement) => cellElement.text().replace(regexpNewLine, '').trim());
 
   if (keyCell.length === 0) {
     return [];
@@ -50,13 +50,13 @@ function extractTable($) {
   const valueCell = Array.from(
     $(
       hasThead
-        ? "tbody td"
-        : "tr:not(:first-of-type) td, tr:not(:first-of-type) th"
+        ? 'tbody td'
+        : 'tr:not(:first-of-type) td, tr:not(:first-of-type) th'
     )
   )
     .map(html)
     .map(load)
-    .map((cellElement) => cellElement.text().replace(regexpNewLine, "").trim());
+    .map((cellElement) => cellElement.text().replace(regexpNewLine, '').trim());
 
   if (valueCell.length === 0) {
     return [];
